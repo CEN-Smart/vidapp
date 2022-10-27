06073a4bd05e317e38f0b8efb74a4f67131bb61c
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2022-10-28 00:02:51 +0100

    Add mobile nav

--- App.js
+++ App.js
@@ -3,3 +3,18 @@
 const yearEl = document.querySelector(".year");
 const newDate = new Date().getFullYear();
 yearEl.innerText = newDate;
+// todo Making the Mobile Nav work
+
+const mobileNavBtn = document.querySelector(".btn-open");
+const navEl = document.querySelector(".nav");
+mobileNavBtn.addEventListener("click", function (e) {
+	e.preventDefault();
+	navEl.classList.toggle("nav-open");
+});
+const linksEl = document.querySelectorAll(".nav-link");
+linksEl.forEach((link) =>
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+		if (link.classList.contains("nav-link")) navEl.classList.toggle("nav-open");
+	})
+);
