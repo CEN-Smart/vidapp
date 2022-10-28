132403f4ff606e57765bdf9d2e9460daf2211c80
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2022-10-28 15:43:20 +0100

    Add sticky nav

--- App.js
+++ App.js
@@ -22,3 +22,21 @@ linksEl.forEach((link) =>
 ctaBtn.addEventListener("click", function () {
 	navEl.classList.toggle("nav-open");
 });
+// todo sticky navigation
+const sectionEl = document.querySelector(".accordian");
+const obs = new IntersectionObserver(
+	function (entries) {
+		const ent = entries[0];
+		if (!ent.isIntersecting) {
+			document.body.classList.add("sticky");
+		} else {
+			document.body.classList.remove("sticky");
+		}
+	},
+	{
+		root: null,
+		threshold: 0,
+		rootMargin: "-6px",
+	}
+);
+obs.observe(sectionEl);
